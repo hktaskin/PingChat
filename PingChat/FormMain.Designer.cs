acaa56322da8f09c27e1e722afa71517dd967af4
Halil Kemal TASKIN <git@hkt.me>
2013-02-15 11:51:16 +0200

    v1.0 Added

---
+++ PingChat/FormMain.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace PingChat
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIP = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.rtxtChat = new System.Windows.Forms.RichTextBox();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.lblLocal = new System.Windows.Forms.Label();
+            this.cmbSourceIP = new System.Windows.Forms.ComboBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Location = new System.Drawing.Point(199, 9);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(76, 13);
+            this.lblIP.TabIndex = 2;
+            this.lblIP.Text = "Destination IP:";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Enabled = false;
+            this.btnConnect.Location = new System.Drawing.Point(396, 4);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(87, 23);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // rtxtChat
+            // 
+            this.rtxtChat.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtxtChat.BackColor = System.Drawing.Color.White;
+            this.rtxtChat.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rtxtChat.Location = new System.Drawing.Point(12, 33);
+            this.rtxtChat.Name = "rtxtChat";
+            this.rtxtChat.ReadOnly = true;
+            this.rtxtChat.Size = new System.Drawing.Size(640, 282);
+            this.rtxtChat.TabIndex = 6;
+            this.rtxtChat.Text = "";
+            this.rtxtChat.WordWrap = false;
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtMsg.Location = new System.Drawing.Point(12, 323);
+            this.txtMsg.MaxLength = 256;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(588, 24);
+            this.txtMsg.TabIndex = 7;
+            this.txtMsg.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMsg_KeyPress);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Location = new System.Drawing.Point(606, 321);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(46, 23);
+            this.btnSend.TabIndex = 8;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(281, 6);
+            this.txtIP.MaxLength = 15;
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(109, 20);
+            this.txtIP.TabIndex = 3;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(486, 4);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(46, 23);
+            this.btnTest.TabIndex = 5;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // lblLocal
+            // 
+            this.lblLocal.AutoSize = true;
+            this.lblLocal.Location = new System.Drawing.Point(12, 9);
+            this.lblLocal.Name = "lblLocal";
+            this.lblLocal.Size = new System.Drawing.Size(57, 13);
+            this.lblLocal.TabIndex = 0;
+            this.lblLocal.Text = "Source IP:";
+            // 
+            // cmbSourceIP
+            // 
+            this.cmbSourceIP.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSourceIP.FormattingEnabled = true;
+            this.cmbSourceIP.Location = new System.Drawing.Point(75, 6);
+            this.cmbSourceIP.Name = "cmbSourceIP";
+            this.cmbSourceIP.Size = new System.Drawing.Size(118, 21);
+            this.cmbSourceIP.TabIndex = 1;
+            this.cmbSourceIP.SelectedIndexChanged += new System.EventHandler(this.cmbSourceIP_SelectedIndexChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 350);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(667, 22);
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(39, 17);
+            this.lblStatus.Text = "Status";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 372);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.cmbSourceIP);
+            this.Controls.Add(this.lblLocal);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.rtxtChat);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.lblIP);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PingChat - hkt.me";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.RichTextBox rtxtChat;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label lblLocal;
+        private System.Windows.Forms.ComboBox cmbSourceIP;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+    }
+}
+
---
+++ PingChat/FormMain.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Net;
+using System.Diagnostics;
+
+/* 
+ * Author: Halil Kemal TASKIN
+ * Web: http://hkt.me
+ * 
+ */
+
+namespace PingChat
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        #region Fields
+
+        int bufferLen = 32;
+
+        byte[] _buffer = new byte[4096];
+
+        EndPoint remoteRawEndPoint = (EndPoint)new IPEndPoint(IPAddress.Any, 0);
+
+        Socket _socket = null;
+
+        string destIP = "", srcIP = "";
+
+        /* DEBUG 
+        int c = 1;
+        long t0;
+        */
+
+        #endregion
+
+        #region Form Methods
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            lblStatus.Text = "Ready. Select a source IP(v4) and write destination IP(v4), click 'Test' and if successful then 'Connect'";
+
+            btnConnect.Enabled = false;
+            btnSend.Enabled = false;
+
+            FillIPList();
+        }
+
+        private void txtMsg_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // If Enter is pressed
+            if (e.KeyChar == 13)
+                btnSend_Click(sender, e);
+        }
+
+        private void cmbSourceIP_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtIP.Text = cmbSourceIP.SelectedItem.ToString().Remove(cmbSourceIP.SelectedItem.ToString().LastIndexOf('.') + 1);
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (btnConnect.Text == "Connect")
+            {
+                if (Start())
+                {
+                    btnSend.Enabled = true;
+                    txtIP.Enabled = false;
+                    btnConnect.Text = "Disconnect";
+                    lblStatus.Text = "Connected...";
+                    txtMsg.Focus();
+                }
+
+            }
+            else
+            {
+                if (Stop())
+                {
+                    btnSend.Enabled = false;
+                    btnConnect.Enabled = false;
+                    txtIP.Enabled = true;
+                    btnConnect.Text = "Connect";
+                    lblStatus.Text = "Disconnected...";
+                }
+
+                MessageBox.Show("Disconnect is not working for now! Please restart the application!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            if (txtIP.Text.Trim() == "")
+            {
+                MessageBox.Show("IP address can not be empty!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            try
+            {
+                Ping p = new Ping();
+
+                PingReply pre = p.Send(txtIP.Text.Trim());
+
+                if (pre.Status == IPStatus.Success)
+                {
+                    btnConnect.Enabled = true;
+                    lblStatus.Text = "Connection seems good! You can connect to that IP";
+
+                    destIP = txtIP.Text.Trim();
+                    srcIP = cmbSourceIP.SelectedItem.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("It seems there is a problem with the destination IP.\r\nPlease check it and try again.", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    lblStatus.Text = "";
+                    return;
+                }
+
+
+            } // try
+            catch (Exception exp)
+            {
+                Debug.WriteLine("ERROR: TestPing: " + exp.Message);
+                lblStatus.Text = "There was an error while testing the connection, check debug console!";
+            }
+
+            //Start();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string msg = txtMsg.Text.Trim();
+
+                if (msg == "")
+                {
+                    MessageBox.Show("Message can not be empty!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+
+                lblStatus.Text = "Sending (pinging) the message";
+
+                Ping p = new Ping();
+
+                string ip = txtIP.Text.Trim();
+
+                byte[] temp = new byte[bufferLen];
+
+                int len = msg.Length - (msg.Length % bufferLen) + bufferLen;
+
+                msg = msg.PadRight(len);
+
+                int rep = len / bufferLen;
+
+                for (int i = 0; i < rep; i++)
+                {
+                    temp = Encoding.UTF8.GetBytes(msg.Substring(32 * i, 32));
+
+                    PingReply pr = p.Send(ip, 0x1338, temp);
+
+                    if (pr.Status != IPStatus.Success)
+                        lblStatus.Text = "!!! " + pr.Status.ToString();
+                }
+
+                rtxtChat.SelectionColor = Color.Black;
+                rtxtChat.AppendText(msg.Trim());
+                rtxtChat.AppendText(Environment.NewLine);
+
+                lblStatus.Text = "Send success";
+                txtMsg.Clear();
+
+            } // try
+            catch (Exception exp)
+            {
+                Debug.WriteLine("ERROR: PingSend: " + exp.Message);
+                lblStatus.Text = "There was an error while sending the ping, check debug console!";
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        bool Start()
+        {
+            try
+            {
+                _socket = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp);
+
+                IPAddress ip = IPAddress.Parse(cmbSourceIP.SelectedItem.ToString().Trim());
+
+                IPEndPoint ipEP = new IPEndPoint(ip, 0);
+
+                _socket.Bind(ipEP);
+
+                int rcvall;
+                
+                unchecked
+                {
+                    rcvall = (int)2550136833;
+                }
+
+                _socket.IOControl(rcvall, new byte[] { 1, 0, 0, 0 }, new byte[] { 0, 0, 0, 0 });
+
+                _socket.ReceiveBufferSize = _buffer.Length;
+
+                _socket.BeginReceiveFrom(_buffer, 0, _buffer.Length, SocketFlags.None, ref remoteRawEndPoint, new AsyncCallback(ResultArrived), _socket);
+
+                AppendText("Started..." + Environment.NewLine);
+
+                /* DEBUG
+                t0 = DateTime.Now.Ticks;
+                */
+
+                return true;
+
+            } // try
+            catch (Exception exp)
+            {
+                Debug.WriteLine("ERROR: Start: " + exp.Message);
+                return false;
+            }
+        }
+
+        void ResultArrived(IAsyncResult iar)
+        {
+            /* DEBUG
+            long t1 = DateTime.Now.Ticks;
+            Debug.WriteLine("Result is arrived! " + (t1-t0).ToString().PadLeft(10) + " " + c++);
+            t0 = t1;
+            */
+
+            Socket _tempsocket = (Socket)iar.AsyncState;
+
+            try
+            {
+                int toRead = _tempsocket.EndReceiveFrom(iar, ref remoteRawEndPoint);
+
+                if (toRead > 20)
+                {
+                    byte[] _ipad = new byte[4];
+                    byte[] _ipad2 = new byte[4];
+
+                    Array.Copy(_buffer, 12, _ipad, 0, 4);
+                    Array.Copy(_buffer, 16, _ipad2, 0, 4);
+
+                    IPAddress _ipadip = new IPAddress(_ipad);
+                    IPAddress _ipadip2 = new IPAddress(_ipad2);
+
+                    string ipad = _ipadip.ToString();
+                    string ipad2 = _ipadip2.ToString();
+
+                    Debug.WriteLine("ipad : " + ipad);
+                    Debug.WriteLine("ipad2: " + ipad2);
+
+                    if (ipad == destIP && ipad2 == srcIP && _buffer[20] == 8 && _buffer[21] == 0)
+                    {
+                        rtxtChat.SelectionColor = Color.Red;
+                        rtxtChat.AppendText(Encoding.UTF8.GetString(_buffer, 28, toRead - 28) + Environment.NewLine);
+                    }
+
+                    //                                      AppendText("00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28" + Environment.NewLine);
+                    AppendText(Environment.NewLine + "                  VH ST LENGT IDENT FLAGS T  P  ChkSm |----S----| |----D----| Ty Co ChkSm IDENT SEQNU |-DATA->" + Environment.NewLine);
+
+                    /*
+                    AppendText("S: " + (new IPAddress(BitConverter.ToInt32(_buffer,12)).ToString()) + 
+                              " D: " + (new IPAddress(BitConverter.ToInt32(_buffer,16)).ToString()) + Environment.NewLine);
+                    */
+                    AppendText(ipad.PadRight(15) + "H: " + BitConverter.ToString(_buffer, 0, 28).Replace('-', ' ') +
+                               " " + Encoding.UTF8.GetString(_buffer, 28, toRead - 28) + Environment.NewLine);
+
+                }
+
+
+            } // try
+            catch (Exception exp)
+            {
+                Debug.WriteLine("ERROR: ResultArrived: " + exp.Message);
+            }
+            finally
+            {
+                _tempsocket.BeginReceiveFrom(_buffer, 0, _buffer.Length, SocketFlags.None, ref remoteRawEndPoint, new AsyncCallback(ResultArrived), _tempsocket);
+            }
+
+        }
+
+        bool Stop()
+        {
+            try
+            {
+                if (_socket != null)
+                {
+                    //_socket.Shutdown(SocketShutdown.Both);
+                    //_socket.Disconnect(true);
+                    //_socket.Close();
+                    //_socket.Dispose();
+                    //_socket = null;
+                }
+
+                return false;
+
+            } // try
+            catch (Exception exp)
+            {
+                Debug.WriteLine("ERROR: Stop: " + exp.Message);
+                return false;
+            }
+
+        }
+
+        void AppendText(string str)
+        {
+            try
+            {
+                Debug.Write(str);
+                //rtxtChat.AppendText(str);
+                //rtxtChat.SelectionStart = rtxtChat.Text.Length;
+
+            } // try
+            catch (Exception exp)
+            {
+                Debug.WriteLine("ERROR: AppendText: " + exp.Message);
+            }
+
+        }
+
+        void FillIPList()
+        {
+            IPHostEntry ipHE = Dns.GetHostEntry(Dns.GetHostName());
+
+            cmbSourceIP.Items.Clear();
+
+            foreach (IPAddress _ip in ipHE.AddressList)
+                if (_ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    cmbSourceIP.Items.Add(_ip.ToString());
+                }
+
+            cmbSourceIP.SelectedIndex = 0;
+        }
+
+        #endregion
+
+    }
+}
